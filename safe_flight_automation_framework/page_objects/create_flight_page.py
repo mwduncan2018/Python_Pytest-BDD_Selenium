b80a40439c2aac75d347664e2ec6b33154780a4b
Michael Duncan <mwduncan2018@gmail.com>
2019-07-14 20:44:13 -0400

    updates

---
+++ safe_flight_automation_framework/page_objects/create_flight_page.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Page-object model for the create flight page.
+
+Usage:
+
+	python3 create_flight_page.py
+
+On Mac/Linux, remember to use the csmod command so the she-bang will work.
+
+	chmod +x words.py
+"""
+
+import sys
+from urllib.request import urlopen
+
+def class CreateFlight:
+
+	@staticmethod
+	def print_stuff(x):
+		print(x)
+
+
+def main():
+	print("main executing")
+
+
+if __name__ == '__main__':
+	main()
+	CreateFlight.print_stuff("testing")
+	
